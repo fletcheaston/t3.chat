c0bca1665488492303d41371954864a68b43983e
Fletcher Easton <fletcheaston@gmail.com>
2025-06-11 23:19:33 -0700

    content + styling for message metadata

--- frontend/src/components/message-content.tsx
+++ frontend/src/components/message-content.tsx
@@ -4,25 +4,30 @@ import { MessageSchema, UserSchema } from "@/api";
 import { useUser } from "@/api/auth";
 import { useMessages, useUserMap } from "@/sync/conversation";
 import { Button } from "@/ui/button";
+import { formatDatetime } from "@/utils";
 
 function MyMessage(props: { message: MessageSchema }) {
     /**************************************************************************/
     /* Render */
     return (
         <div className="flex justify-end">
-            <div className="group relative max-w-[85%] pb-10">
-                <div className="bg-gunmetal text-silver rounded-xl rounded-br-none p-4">
+            <div className="group relative flex w-[85%] justify-end pb-10">
+                <div className="bg-gunmetal text-silver w-fit rounded-xl rounded-br-none p-4">
                     <p className="whitespace-pre-wrap">{props.message.content}</p>
                 </div>
 
                 <div className="absolute right-0 bottom-0 opacity-0 transition-all group-hover:opacity-100">
-                    <Button
-                        size="icon"
-                        className="hover:bg-pantone-light hover:text-gunmetal-dark rounded"
-                        tooltip="Copy message"
-                    >
-                        <CopyIcon />
-                    </Button>
+                    <div className="flex items-center gap-4">
+                        <p className="text-sm">{formatDatetime(props.message.created)}</p>
+
+                        <Button
+                            size="icon"
+                            className="hover:bg-pantone-light hover:text-gunmetal-dark rounded"
+                            tooltip="Copy message"
+                        >
+                            <CopyIcon />
+                        </Button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -43,8 +48,26 @@ function OtherMessage(props: { message: MessageSchema; user: UserSchema }) {
     /**************************************************************************/
     /* Render */
     return (
-        <div className="w-full text-left">
-            <p className="whitespace-pre-wrap">{props.message.content}</p>
+        <div>
+            <div className="group relative pb-10">
+                <p className="whitespace-pre-wrap">{props.message.content}</p>
+
+                <div className="absolute bottom-0 left-0 opacity-0 transition-all group-hover:opacity-100">
+                    <div className="flex items-center gap-4">
+                        <Button
+                            size="icon"
+                            className="hover:bg-pantone-light hover:text-gunmetal-dark rounded"
+                            tooltip="Copy message"
+                        >
+                            <CopyIcon />
+                        </Button>
+
+                        <p className="text-sm">{formatDatetime(props.message.created)}</p>
+
+                        <p className="text-pantone-light text-sm">{props.user.name}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
