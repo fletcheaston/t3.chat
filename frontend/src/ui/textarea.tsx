d0e34f0190844c816d1219c082b1bd2ec57e1856
Fletcher Easton <fletcheaston@gmail.com>
2025-06-15 17:08:17 -0700

    fix font weight in textarea

--- frontend/src/ui/textarea.tsx
+++ frontend/src/ui/textarea.tsx
@@ -8,7 +8,7 @@ function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
             data-slot="textarea"
             rows={1}
             className={cn(
-                "placeholder:text-text-dark flex field-sizing-content min-h-12 w-full resize-none text-sm transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50",
+                "placeholder:text-text-dark flex field-sizing-content min-h-12 w-full resize-none text-sm font-medium transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50",
                 className
             )}
             {...props}
