5e4db931042d521d4591c2a15a7d8c7f70ff4987
Fletcher Easton <fletcheaston@gmail.com>
2025-06-16 23:07:59 -0700

    retag image, minor env var updates

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,8 +1,3 @@
 import { z } from "zod";
 
-export const apiUrl = z
-    .string()
-    .url()
-    .parse(import.meta.env.VITE_API_URL);
-
 export const githubOauthClientId = z.string().parse(import.meta.env.VITE_GITHUB_OAUTH_CLIENT_ID);
