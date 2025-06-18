cceb27838adab43def7bd81e7d7336dbae9cfda7
Fletcher Easton <fletcheaston@gmail.com>
2025-06-17 19:56:59 -0700

    cache to use the right redis host

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -171,7 +171,7 @@ CELERY_TASK_TIME_LIMIT = 60
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": f"redis://{SETTINGS.REDIS_HOST}:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
